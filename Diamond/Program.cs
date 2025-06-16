f73171ef47b6b0e0a63b85c89ecec53502c17ebe
ooSong <teraswy@naver.com>
2025-06-16 20:43:30 +0900

    Diamon 0.1

---
+++ Diamond/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System;
+using static System.Console;
+
+namespace Diamond
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            DrawDiamond();
+        }
+
+        public static void DrawDiamond()
+        {
+            WriteLine();
+
+            int height = Input.GetHeight();
+
+            WriteLine();
+            Output.PrintDiamondOneLoop(height);
+            WriteLine();
+
+            if (Input.RestartProgram())
+            {
+                DrawDiamond();
+            }
+            else
+            {
+                WriteLine("Thank you for using the diamond drawing program!");
+            }
+        }
+    }
+}
