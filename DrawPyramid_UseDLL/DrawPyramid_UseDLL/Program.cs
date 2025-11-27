879df0c929f4ed1035c18c9bd7d95882ec6bd04d
ooSong <teraswy@naver.com>
2025-11-27 12:32:14 +0900

    ClassLibrary_PrintStar , DrawPyramid_UseDLL 추가
    ClassLibrary_PrintStar , DrawPyramid_UseDLL 추가

---
+++ DrawPyramid_UseDLL/DrawPyramid_UseDLL/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using static System.Console;
+using MyClass_Library;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        Write("Enter the number of rows for the pyramid: ");
+        if (int.TryParse(ReadLine(), out int rows) && rows > 0)
+        {
+            Pyramid.DrawPyramid(rows, true);            
+        }
+        else
+        {
+            WriteLine("Please enter a valid positive integer.");
+        }
+
+    }
+}
