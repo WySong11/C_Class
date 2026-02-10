9085d7516bc206332541698e086dcba8e71dab60
ooSong <teraswy@naver.com>
2026-02-10 11:23:21 +0900

    TestLecture CSV 추가

--- TestLecture/TestLecture/obj/Debug/net8.0/LectureVariable.AssemblyInfo.cs
+++ TestLecture/TestLecture/obj/Debug/net8.0/LectureVariable.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LectureVariable")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c7f6bbf58c2925edb3a931c85bdb6d17e85515c2")]
 [assembly: System.Reflection.AssemblyProductAttribute("LectureVariable")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LectureVariable")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
